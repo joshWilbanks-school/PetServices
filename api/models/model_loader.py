315f2a171ed44fa7e8b6ec157e093752b88b5f73
joshWilbanks-school <jwilban3@charlotte.edu>
2024-10-08 22:14:09 -0400

    feat!: added customer table, schema, and api end points

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import user, user_type, service_provider
+from . import user, user_type, service_provider, customer
 
 from dependencies.database import engine
 
@@ -7,6 +7,7 @@ def index():
     user.Base.metadata.create_all(engine)
     user_type.Base.metadata.create_all(engine)
     service_provider.Base.metadata.create_all(engine)
+    customer.Base.metadata.create_all(engine)
 
 
 
